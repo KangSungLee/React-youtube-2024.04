9547234a293cd275352820fc49b42c8df6e2446f
KangSungLee <rkdtjd1020@naver.com>
2024-04-12 16:00:37 +0900

    youtube

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -6,33 +6,33 @@ import Grid from '@mui/material/Grid';
 import HourglassTopIcon from '@mui/icons-material/HourglassTop';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import VideoCard from "../components/VideoCard";
-
-const keywordUri = `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=25&part=snippet&q=`;
-const popularUri = `https://youtube.googleapis.com/youtube/v3/videos?chart=mostPopular&key=${process.env.REACT_APP_YOUTUBE_API_KEY}&maxResults=25&part=snippet`;
+import { useVideo } from "../api/youtube"
 
 export default function Videos() {
   const { keyword } = useParams();
-  const {isLoading, error, data: videos} = useQuery({
-    queryKey: ['videos', keyword],
-    queryFn: async () => {
-      const uri = keyword ? keywordUri + keyword : popularUri;
-      return axios
-              // .get(`/data/${keyword ? 'search' : 'popular'}.json`)
-              .get(uri)
-              .then(res => res.data.items);
-              // .then(res => keyword ? res.data.items.shift() : res.data.items);
-    },
-    staleTime: 1000 * 60 * 1,       // 1분, ms 단위로 지정할 수 있음
-  });
+  // const {isLoading, error, data: videos} = useQuery({
+  //   queryKey: ['videos', keyword],
+  //   queryFn: async () => {
+  //     const uri = keyword ? keywordUri + keyword : popularUri;
+  //     return axios
+  //             // .get(`/data/${keyword ? 'search' : 'popular'}.json`)
+  //             .get(uri)
+  //             .then(res => res.data.items);
+  //             // .then(res => keyword ? res.data.items.shift() : res.data.items);
+  //   },
+  //   staleTime: 1000 * 60 * 1,       // 1분, ms 단위로 지정할 수 있음
+  // });
+  const {isLoading, error, videos} = useVideo(keyword);
+
 
   return (
     <>
-      {isLoading && <p><HourglassTopIcon /> Loading...</p>}  {/*로딩중*/}
-      {error && <p><WarningAmberIcon /> Something is wrong!!!</p>}  {/*에러*/}
+      {isLoading && <p><HourglassTopIcon /> Loading...</p>}
+      {error && <p><WarningAmberIcon /> Something is wrong!!!</p>}
       {videos && (
         <Grid container spacing={1}>
-          {videos.map(video => (
-            <Grid item xs={12} sm={6} md={4} lg={3}>
+          {videos.map((video, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
               <VideoCard video={video} />
             </Grid>
           ))}
