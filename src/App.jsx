32ae7a417abdd9e5cdb7ad1daba75c99f1eaa638
KangSungLee <rkdtjd1020@naver.com>
2024-04-19 17:28:35 +0900

    택배조회기능 미완

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
 import SearchHeader from './components/SearchHeader';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import DeliveryTracker from './test/DeliveryTracker';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +12,7 @@ function App() {
       <SearchHeader />
       <QueryClientProvider client={queryClient}>
         <Outlet />
+        <DeliveryTracker />
       </QueryClientProvider>
     </AuthContextProvider>
   );
